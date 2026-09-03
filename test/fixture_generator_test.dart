1fdbc802dee4452a6e265eb23fa89800219d02da
ZMilaStudio <levent.ua@gmail.com>
2026-09-03 17:24:27 +0300

    fix: use explicit double strength in fixture test

--- test/fixture_generator_test.dart
+++ test/fixture_generator_test.dart
@@ -4,7 +4,11 @@ import 'package:test/test.dart';
 void main() {
   final clubs = List.generate(
     8,
-    (i) => Club(id: 'c$i', name: 'Club $i', strength: 60 + i),
+    (i) => Club(
+      id: 'c$i',
+      name: 'Club $i',
+      strength: (60 + i).toDouble(),
+    ),
   );
 
   test('8-club double round robin has 56 matches and balanced home/away', () {
