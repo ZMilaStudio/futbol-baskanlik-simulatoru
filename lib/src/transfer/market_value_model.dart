ba09860958c3de937628d83def1c29b099e2a114
ZMilaStudio <levent.ua@gmail.com>
2026-09-04 01:44:17 +0300

    M7: make contract term affect market value

--- lib/src/transfer/market_value_model.dart
+++ lib/src/transfer/market_value_model.dart
@@ -4,7 +4,10 @@ import '../player/player.dart';
 class MarketValueModel {
   const MarketValueModel();
 
-  Money value(Player player) {
+  Money value(
+    Player player, {
+    int? contractYearsRemaining,
+  }) {
     final quality = (player.ability - 45.0).clamp(0.0, 50.0);
     final baseUnits = (quality * quality * 6000).round();
 
@@ -23,6 +26,19 @@ class MarketValueModel {
     var value = Money.fromUnits(baseUnits < 250000 ? 250000 : baseUnits);
     value = value.scaleBasisPoints(ageBps);
     value = value.scaleBasisPoints(potentialBps);
+
+    if (contractYearsRemaining != null) {
+      final contractBps = switch (contractYearsRemaining) {
+        <= 0 => 2500,
+        1 => 7000,
+        2 => 9000,
+        3 => 10300,
+        4 => 11000,
+        _ => 11500,
+      };
+      value = value.scaleBasisPoints(contractBps);
+    }
+
     return value.max(const Money.fromUnits(250000));
   }
 }
