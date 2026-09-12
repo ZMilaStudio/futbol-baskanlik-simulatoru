d0d13f2ee3a94fec454cbcc71d2bb054eaaf3943
ZMilaStudio <levent.ua@gmail.com>
2026-09-12 14:36:49 +0300

    fix: export M47 facility state types

--- lib/facility_sponsor_crisis_runtime_composition.dart
+++ lib/facility_sponsor_crisis_runtime_composition.dart
@@ -1,1 +1,5 @@
 export 'src/crisis/facility_sponsor_crisis_runtime_composition.dart';
+export 'src/facility/academy_facility.dart' show AcademyFacilityState;
+export 'src/facility/stadium_facility.dart' show StadiumFacilityState;
+export 'src/facility/training_ground_facility.dart'
+    show TrainingGroundFacilityState;
